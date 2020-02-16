0b778fcb0ed2e4667f3c92cdbe13bf5ea31ad5a4
AlehZahorski <git3371m@gmail.com>
2020-02-16 11:51:24 +0100

    add ru and eng

--- js/config_language.js
+++ js/config_language.js
@@ -1,29 +1,29 @@
-var arrLang = new Array();
-      arrLang['en'] = new Array();
-      arrLang['pl'] = new Array();
-      arrLang['ru'] = new Array();
+// var arrLang = new Array();
+//       arrLang['en'] = new Array();
+//       arrLang['pl'] = new Array();
+//       arrLang['ru'] = new Array();
 
-      //Polish content
-      arrLang['pl']['technologies'] = 'Technologie';
-      arrLang['pl']['my_projects'] = 'Moje projekty';
-      arrLang['pl']['about_me'] = 'O mnie';
-      arrLang['pl']['contacts'] = 'Kontakty';
+//       //Polish content
+//       arrLang['pl']['technologies'] = 'Technologie';
+//       arrLang['pl']['my_projects'] = 'Moje projekty';
+//       arrLang['pl']['about_me'] = 'O mnie';
+//       arrLang['pl']['contacts'] = 'Kontakty';
 
 
-      // English content
-      arrLang['en']['technologies'] = 'Technolohies';
-      arrLang['en']['my_projects'] = 'My projects';
-      arrLang['en']['about_me'] = 'About me';
-      arrLang['en']['contacts'] = 'Contacts';
+//       // English content
+//       arrLang['en']['technologies'] = 'Technolohies';
+//       arrLang['en']['my_projects'] = 'My projects';
+//       arrLang['en']['about_me'] = 'About me';
+//       arrLang['en']['contacts'] = 'Contacts';
   
 
-      // Russian content
-      arrLang['ru']['technologies'] = 'Технологии';
-      arrLang['ru']['my_projects'] = 'Мои проекты';
-      arrLang['ru']['about_me'] = 'Обо мне';
-      arrLang['ru']['contacts'] = 'Контакты';
+//       // Russian content
+//       arrLang['ru']['technologies'] = 'Технологии';
+//       arrLang['ru']['my_projects'] = 'Мои проекты';
+//       arrLang['ru']['about_me'] = 'Обо мне';
+//       arrLang['ru']['contacts'] = 'Контакты';
  
   
    
   
-      // Process translation
+//       // Process translation
