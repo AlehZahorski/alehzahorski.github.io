b7f11ad5ee34cc38de26380b02e9124296dbc004
AlehZahorski <git3371m@gmail.com>
2020-02-24 14:28:05 +0100

    add random facts

--- js/animation.js
+++ js/animation.js
@@ -8,39 +8,66 @@
   })(jQuery); 
 //animacja przycisku
 
-// собираем все якоря; устанавливаем время анимации и количество кадров
+//якоря
 const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
       animationTime = 500,
       framesCount = 20;
-
 anchors.forEach(function(item) {
-  // каждому якорю присваиваем обработчик события
   item.addEventListener('click', function(e) {
-    // убираем стандартное поведение
     e.preventDefault();
-    
-    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
     let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
-    
-    // запускаем интервал, в котором
     let scroller = setInterval(function() {
-      // считаем на сколько скроллить за 1 такт
       let scrollBy = coordY / framesCount;
-      
-      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-      // и дно страницы не достигнуто
       if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-        // то скроллим на к-во пикселей, которое соответствует одному такту
         window.scrollBy(0, scrollBy);
       } else {
-        // иначе добираемся до элемента и выходим из интервала
         window.scrollTo(0, coordY);
         clearInterval(scroller);
       }
-    // время интервала равняется частному от времени анимации и к-ва кадров
     }, animationTime / framesCount);
   });
 });
+
+
+//losowanie ciekawostek
+var i = 0;
+var answers = [
+"Ziemia jest jedyną planeta w naszym układzie sloniecznym, której nazwa nie pochodzi od imieniu boga.",
+"W Chinach zakazane jest przytulanie drzwi",
+"Potrzeba ponad 60 tysięcy lat, aby obejrzeć wszystkie filmiki zamieszczone obecnie na Youtube.",
+"Dwie trzecie populacji na świecie nigdy nie widziało śniegu.",
+"Hymn Królestwa Hiszpanii nie ma słów.",
+'Howard Hughes – amerykański miliarder i konstruktor lotniczy kupił kasyno tylko po to, by usunąć z niego neon, który widział ze swojego okna.',
+"Jeden dzień na Ziemi trwa dłużej niż jeden rok na Wenus.",
+"Wydry podczas snu trzymają się nawzajem za łapki, żeby nie odpłynąć od siebie.",
+"Istnieje więcej możliwości ruchów w szachach niż atomów we wszechświecie.",
+"Na twoim ciele znajduje się więcej organizmów żywych niż ludzi na ziemi.",
+"Przeciętny wysportowany człowiek przechodzi w swoim życiu dystans równy pięciu pokrążeniom ziemi.",
+"„Dollar Babies” to inicjatywa wymyślona przez Stephena Kinga, żeby wspierać początkujących filmowców. Autor udziela takim osobom prawa do adaptacji jednej ze swoich książek za symbolicznego dolara. Zastrzeżenie jest jednak aby filmy nie były szeroko rozpowszechniane, oprócz festiwali filmowych – tak, żeby twórca mógł zostać dostrzeżony przez krytyków.",
+"Aby wypić całą krew z organizmu człowieka potrzeba by około miliona komarów."
+
+];
+
+$(function () {
+$('.panel-text').click(function () {
+i = Math.floor(Math.random() * answers.length)
+showText(this, answers[i]);
+}); ;
+function showText(panel, text) {
+var panel = $(panel);
+var msg = $('.message', panel);
+panel.fadeOut(500, function () {
+msg.html(text);
+panel.fadeIn(500);
+});
+}
+});
+function showText(){
+  i = Math.floor(Math.random() * answers.length);
+  message.innerHTML = answers[i];
+  setTimeout(showText, Math.random() * 20000);
+}
+showText();
 
 
 
