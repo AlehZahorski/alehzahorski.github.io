f611725b69933e6faf7d62644ad64825d8a4da6f
AlehZahorski <git3371m@gmail.com>
2020-03-02 18:56:04 +0100

    code rewiev

--- js/config_language.js
+++ js/config_language.js
@@ -1,29 +1,120 @@
-// var arrLang = new Array();
-//       arrLang['en'] = new Array();
-//       arrLang['pl'] = new Array();
-//       arrLang['ru'] = new Array();
+var arrLang = new Array();
+      arrLang['en'] = new Array();
+      arrLang['pl'] = new Array();
+      arrLang['ru'] = new Array();
 
-//       //Polish content
-//       arrLang['pl']['technologies'] = 'Technologie';
-//       arrLang['pl']['my_projects'] = 'Moje projekty';
-//       arrLang['pl']['about_me'] = 'O mnie';
-//       arrLang['pl']['contacts'] = 'Kontakty';
+      //Polish content
+      arrLang['pl']['technologies'] = 'Technologie';
+      arrLang['pl']['my_projects'] = 'Moje projekty';
+      arrLang['pl']['about_me'] = 'O mnie';
+      arrLang['pl']['contacts'] = 'Kontakty';
+      arrLang['pl']['welcome_to__my_page'] = 'Cześć. Cieszę się że tu jesteś. Zapoznaj się z moją stroną, a poznasz mnię. Witam w moim świecie.';
+      arrLang['pl']['welcome_to__my_page_button'] = 'Zacząc przygode';
+      arrLang['pl']['section_1'] = 'Technologie';
+      arrLang['pl']['section_1_first-text'] = 'Wiem że to raczej jest nudne, lecz muszę zaprezentować technologię w których głównie pracuję, więc słuchaj:';
+      arrLang['pl']['section_1_second-text'] = 'Myślę że podstawą WEB-developmenta jest, wcześniej była i zawsze będzie dobra znajomość semantyki HTML.';
+      arrLang['pl']['section_1_three-text'] = 'Drugą bardzo ważna rzeczą jest prawidłowe stylowanie, tylko naprawdę dobry developer potrafi zbudować stronę odpowiedniej jakości.';
+      arrLang['pl']['section_1_four-text'] = "Trzecim słupem na którym trzyma się WEB jest JavaScript, za pomocą JS'a da się przepisać logikę strony oraz przedstawić się w jak najliepszym świecie przed użytkownikiem poprzez animacje.";
+      arrLang['pl']['section_2'] = "Projekty";
+      arrLang['pl']['section_2_presentation'] = "Przedstawiam do wzoru kilka swoich projektow";
+      arrLang['pl']['section_2_i-hope'] = "am nadzeje że do tej pory wszystko Ci się podoba, więc czas trochę się zrelaksować i poznać coś nowego.";
+      arrLang['pl']['section_2_interesting-fact'] = "Losowa ciekawostka:";
+      arrLang['pl']['section_2.1'] = "STANDARDY WEB'a";
+      arrLang['pl']['section_2.1_first-text'] = " Semantyka w HTML. Nie wszyscy dostrzegają zasad SEO, i to jest poważny problem. Bez SEO twoją strona będzie mniej widoczna dla wyszukiwarek, a przy wyświetlaniu w google będzie widoczne coś randomowe, a nie to czego byś chciałeś. Tak że dostrzegajmy zasad!";
+      arrLang['pl']['section_2.1_second-text'] = " Drugim bardzo ważnym elementa budowania strony jest RESPONSIVE, czyli robienie CSS tak jak trzeba, a nie tak jak się chce. Dobrze zaprojektowana strona będzie dobrze się wyświetlała na wszystkich ekranach.";
+      arrLang['pl']['section_2.1_three-text'] = "Trzecią ważna rzeczą jest INTUITUVE, czyli strona ma być zaprojektowana w taki sposób, żeby użytkownik nie miał problemu z dostępnością do informacji, to już jest UX/UI designe. Im lepej zaprojektowana strona, tym jest przyjemniej z niej korzystać.";
+      arrLang['pl']['section_3'] = "O mnie";
+      arrLang['pl']['section_3_first-text'] = "Dobra, teraz opowiem trochę o sobie. Nazywam się Aleh. Zajmuje się tworzeniem aplikacji webowych, w tym równiez stron internetowych. Uważam że główne to robić swoje dobrze. Nie ważnie ile masz czasu, wszystko ma być robione z pasją.";
+      arrLang['pl']['section_3_second-text'] = "Mogę zrobić stronę od początku (designe) do rozmieszczenia w internecie. Na co dzień się uczę, studjeie inzynenie programowania. Gram na basię, czytam literaturę jak klasyczną tak równiez techniczna, ponieważ chce rozszerzyć swoją wiedzę odnośnie WEB'a.";
+      arrLang['pl']['section_3_three-text'] = "Na wszelki wypadek dołączam również swoje CV.";
+      arrLang['pl']['section_3_four-text'] = "Cieszę się że byliśmy razem przez całą podróz. Otóż czas podzielić się forma dla kontaktu. Dziękuję Ci. Do zobaczenia.";
+      arrLang['pl']['section_3_button'] = "Pobrac CV";
+      arrLang['pl']['section_4'] = "Contacts";
+      arrLang['pl']['section_4_1'] = "Wyslij";
+      arrLang['pl']['section_4_2'] = "Telefon kontaktowy:";
+      arrLang['pl']['section_4_3'] = "Poczta";
+      arrLang['pl']['section_4_4'] = "Polska";
+      arrLang['pl']['section_4_5'] = "Bialorus/Viber";
+      
 
 
-//       // English content
-//       arrLang['en']['technologies'] = 'Technolohies';
-//       arrLang['en']['my_projects'] = 'My projects';
-//       arrLang['en']['about_me'] = 'About me';
-//       arrLang['en']['contacts'] = 'Contacts';
+      // English content
+      arrLang['en']['technologies'] = 'Technologies';
+      arrLang['en']['my_projects'] = 'My projects';
+      arrLang['en']['about_me'] = 'About me';
+      arrLang['en']['contacts'] = 'Contacts';
+      arrLang['en']['welcome_to__my_page'] = 'Hi. Glad to see you reading my page. I hope this allows you to know me a little better.';
+      arrLang['en']['welcome_to__my_page_button'] = "Let's start";
+      arrLang['en']['section_1'] = 'Technologies';
+      arrLang['en']['section_1_first-text'] = 'I understand that it seems to be boring, but I really should introduce you to the technologies that helping me at work.';
+      arrLang['en']['section_1_second-text'] = 'Let’s start with a markup language, HTML exactly. This one is the basis of the entire web application.';
+      arrLang['en']['section_1_three-text'] = 'The second one, but no less important part of the web application is CSS. Using CSS we set the page style.';
+      arrLang['en']['section_1_four-text'] = 'The third pillar on which all web applications are held is JavaScript. Using JS we set the logic of the page’s behavior.';
+      arrLang['en']['section_2'] = 'Projects';
+      arrLang['en']['section_2_presentation'] = 'Some of my projects';
+      arrLang['en']['section_2_i-hope'] = 'I hope you are satisfied with my presentation. It’s time to relax and learn something new.';
+      arrLang['en']['section_2_interesting-fact'] = 'Random fact:';
+      arrLang['en']['section_2.1'] = "WEB's standarts";
+      arrLang['en']['section_2.1_first-text'] = "The rules of semantic HTML are very important, proper page layout is just like competent speech. You can do without it, but using it you will be presented in a much more favorable light. And for the browser, semantic, strictly typed layout is an absolute must-have.";
+      arrLang['en']['section_2.1_second-text'] = "The second, but no less important is the adaptive page-proofs rule, it is often called simply - RWD. Properly styled page will look great on every device.";
+      arrLang['en']['section_2.1_three-text'] = "The third rule of high-quality page-proofs is accessibility, in other words, an intuitive interface. This is where the UX / UI design comes into play. The definition itself appeared not so long ago, but good developers have been using it for a very long time.";
+      arrLang['en']['section_3'] = "About me";
+      arrLang['en']['section_3_first-text'] = "About me";
+      arrLang['en']['section_3_first-text'] = " Well, now I think it's time to tell something about me. My name, as you already understood, is Oleg. I am working in web development. The knowledge I was being collecting for a long time, by trial and error, are helping me in that.";
+      arrLang['en']['section_3_second-text'] = "I can make a page from the beginning (layout design) and lead it to its logical conclusion (placement on the Internet). I study at WWSI (Warsaw Higher School of Informatics), at the Faculty of Programming Engineering.  My hobbies are playing bass, reading (classic / technical literature) and the security of web applications.";
+      arrLang['en']['section_3_three-text'] = "Just in case, I pin my resume here.";
+      arrLang['en']['section_3_four-text'] = "I am very glad that we came this path together. I think this is the best time to share contact information. Thanks. See you soon.";
+      arrLang['en']['section_3_button'] = "Download CV";
+      arrLang['en']['section_4'] = "Contacts";
+      arrLang['en']['section_4_1'] = "Push";
+      arrLang['en']['section_4_2'] = "Telephones";
+      arrLang['en']['section_4_3'] = "Mail";
+      arrLang['en']['section_4_4'] = "Polish";
+      arrLang['en']['section_4_5'] = "Belarus/Viber";
   
 
-//       // Russian content
-//       arrLang['ru']['technologies'] = 'Технологии';
-//       arrLang['ru']['my_projects'] = 'Мои проекты';
-//       arrLang['ru']['about_me'] = 'Обо мне';
-//       arrLang['ru']['contacts'] = 'Контакты';
- 
-  
+      // Russian content
+      arrLang['ru']['technologies'] = 'Технологии';
+      arrLang['ru']['my_projects'] = 'Мои проекты';
+      arrLang['ru']['about_me'] = 'Обо мне';
+      arrLang['ru']['contacts'] = 'Контакты';
+      arrLang['ru']['welcome_to__my_page'] = 'Привет. Рад тебя видеть! Эта страница поможет тебе познакомиться со мной получше. Добро пожаловать!';
+      arrLang['ru']['welcome_to__my_page_button'] = 'Вперёд';
+      arrLang['ru']['section_1'] = 'Технологии';
+      arrLang['ru']['section_1_first-text'] = 'Я понимаю, что это может показаться нудным, однако я должен познакомить тебя с технологиями которые помогают мне в работе.';
+      arrLang['ru']['section_1_second-text'] = 'Начать я думаю стоит с языка разметки, а именно HTML. Ведь именно этот язык является основой всего веб-приложения.';
+      arrLang['ru']['section_1_three-text'] = 'Второй, но не менее важной частью веб-приложения является CSS, с помощью CSS мы задаем стиль страницы.';
+      arrLang['ru']['section_1_four-text'] = 'Третим столбом на котором держатся все веб-приложения является JavaScript, с помощью JS мы задаем логику поведения страницы.';
+      arrLang['ru']['section_2'] = 'Проекты';
+      arrLang['ru']['section_2_presentation'] = 'Несколько моих проектов';
+      arrLang['ru']['section_2_i-hope'] = 'Надеюсь до сих пор ты остаешься доволен нашим путешествием, время расслабиться и узнать что-то новое.';
+      arrLang['ru']['section_2_interesting-fact'] = 'Случайный факт: ';
+      arrLang['ru']['section_2.1'] = "Стандарты веб-разработки";
+      arrLang['ru']['section_2.1_first-text'] = "Правила семантического HTML очень важны, грамотная разметка страницы сродни грамотной речи. Без нее можно, но с ней вы представляетесь в куда более выгодном свете. А для браузера семантическая, строго типизированная верстка это musthave.";
+      arrLang['ru']['section_2.1_second-text'] = "Вторым, но не менее важным правилом является правило адаптивной верстки, ее же часто называют просто - RWD. Грамотно застилизированная страница будет выглядеть отлично абсолютно на всех устройствах.";
+      arrLang['ru']['section_2.1_three-text'] = "Третьим правилом качественной верстки является доступность, другими словами интуитивно понятный интерфейс. Здесь уже в игру вступают UX/UI дизайн. Само определение появилось относительно недавно, однако хорошие разработчики руководствуются этим уже очень давно.";
+      arrLang['ru']['section_3'] = "Обо мне";
+      arrLang['ru']['section_3_first-text'] = "Хорошо, теперь думаю самое время рассказать немного о себе. Меня, как вы уже поняли, зовут Олег. Я занимаюсь веб-разработкой. В этом мне помогают знания которые собирались и собираются в одно целое на протяжении какого-то времени, методом проб и ошибок.";
+      arrLang['ru']['section_3_second-text'] = "Я могу сделать страницу от начала(дизайн макета) и провести ее до логического завершения(размещение в интернете). Я учусь в WWSI(Варшавская высшая школа информатики), на факультете инжинерия программирования.  Как свое хобби могу отметить игру на бас-гитаре, чтение литературы(классика/техническая) и безопасность веб-приложений.";
+      arrLang['ru']['section_3_three-text'] = "На всякий случай прикрепляю тут свое резюме. ";
+      arrLang['ru']['section_3_four-text'] = "Я очень рад, что мы прошли вместе такой пусть и не большой но все же интересный путь. Сейчас я думаю самое лучшее время поделиться контактными данными. Спасибо. До встречи.";
+      arrLang['ru']['section_3_button'] = "Скачать CV";
+      arrLang['ru']['section_4'] = "Контакты";
+      arrLang['ru']['section_4_1'] = "Отправить";
+      arrLang['ru']['section_4_2'] = "Контактные телефоны";
+      arrLang['ru']['section_4_3'] = "Почта";
+      arrLang['ru']['section_4_4'] = "Польша";
+      arrLang['ru']['section_4_5'] = "Беларусь";
+      
    
   
-//       // Process translation
+      // Process translation
+      $(function() {
+        $('.translate').click(function() {
+          var lang = $(this).attr('id');
+  
+          $('.lang').each(function(index, item) {
+            $(this).text(arrLang[lang][$(this).attr('key')]);
+          });
+        });
+      });
