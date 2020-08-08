30427dc3c1decd4a5725bb557e71eedc6d2d7a37
AlehZahorski <git3371m@gmail.com>
2020-08-08 10:13:14 +0200

    add 4 project

--- js/main.js
+++ js/main.js
@@ -59,15 +59,17 @@ imgCloseWin.addEventListener('click', function(){
 // finish password's window
 
 // 1)создать див которая в котором будет класс скрывающий его в нативном режиме ГОТОВО
-// 2)добавить стили как в примере с паролем
-// 3)добавить ссылки на страницы(убрать их же с хтмл)
-// 4)добавить описание для каждой ссылки и возможность открыть гитхаб(код) 
+// 2)добавить стили как в примере с паролем ГОТОВО
+// 3)добавить ссылки на страницы(убрать их же с хтмл) ГОТОВО
+// 4)добавить описание для каждой ссылки и возможность открыть гитхаб(код)  ГОТОВО
 // или страницу в онлайн режиме
-// 5)добавить анимацию в окошко открывающееся
+// 5)добавить анимацию в окошко открывающееся 
+
 
 const btnProject1 = document.getElementById('btn-project1');
 const btnProject2 = document.getElementById('btn-project2');
 const btnProject3 = document.getElementById('btn-project3');
+const btnProject4 = document.getElementById('btn-project4');
 const projectDisplayImage = document.getElementById('projectDisplayImage');
 const closeWindowProject = document.getElementById('closeWindowProject');
 
@@ -79,6 +81,7 @@ const descriptionProject = document.getElementById('descriptionProject');
 const firstProjectImage = document.getElementById('firstProjectImage');
 const secondProjectImage = document.getElementById('secondProjectImage');
 const thirdProjectImage = document.getElementById('thirdProjectImage');
+const fourProjectImage = document.getElementById('fourProjectImage');
 const thirdLi = document.getElementById('thirdLi');
 const secondLi = document.getElementById('secondLi');
 
@@ -87,6 +90,7 @@ const clickForOpenWindow1 = () =>{
     projectWindow.classList.remove('displayNoneProject');
     firstProjectImage.classList.remove('displayNoneProject');
     thirdProjectImage.classList.add('displayNoneProject');
+    fourProjectImage.classList.add('displayNoneProject');
     secondProjectImage.classList.add('displayNoneProject');
     descriptionProject.textContent = 'W tym projekcie głównym celem dla mnie było nauczyć się posługiwać API, czyli wprost mówiąc w jaki sposób dane się pobierają i w jaki sposób się wyświetlają. Myślę że dało się ogarnąć ten temat w miarę dobrze.'
     firstButton.href = "https://alehzahorski.github.io/your-weather/";
@@ -100,6 +104,7 @@ const clickForOpenWindow2 = () =>{
     firstProjectImage.classList.add('displayNoneProject');
     secondProjectImage.classList.remove('displayNoneProject');
     thirdProjectImage.classList.add('displayNoneProject');
+    fourProjectImage.classList.add('displayNoneProject');
     descriptionProject.textContent = 'Ten project zacząłem robić jeszcze w środku 2019 roku i będę go rozwiał dalej, ponieważ jest to dobry teram żeby sobie przećwiczyć Javascript(czym teraz głównie się zajmuje). W planach uzupełnić sekcje "message" i później próbować dodawać użytkowników.'
     firstButton.href = "https://alehzahorski.github.io/social_network/";
     secondButton.href = "https://github.com/AlehZahorski/social_network";
@@ -112,11 +117,25 @@ const clickForOpenWindow3 = () =>{
     firstProjectImage.classList.add('displayNoneProject');
     secondProjectImage.classList.add('displayNoneProject');
     thirdProjectImage.classList.remove('displayNoneProject');
-    descriptionProject.textContent = 'To jest dość prosty layout designe ktorego pobralem z internetu, na tym sobie przećwiczyłem bootstrapa, nie chciałem rozbudowywać ten project ponieważ teraz bardziej się skupilem na logice js. '
+    fourProjectImage.classList.remove('displayNoneProject');
+    descriptionProject.textContent = 'To jest dość prosty layout designe ktorego pobralem z internetu, na tym sobie przećwiczyłem bootstrapa. '
     firstButton.href = "https://alehzahorski.github.io//new-providence-website/";
     secondButton.href = "https://github.com/AlehZahorski//new-providence-website";
     secondLi.textContent = "CSS: Bootstrap"
     thirdLi.textContent = "Javascript: jQuery"
+}
+
+const clickForOpenWindow4 = () =>{
+    projectWindow.classList.remove('displayNoneProject');
+    firstProjectImage.classList.add('displayNoneProject');
+    secondProjectImage.classList.add('displayNoneProject');
+    thirdProjectImage.classList.add('displayNoneProject');
+    fourProjectImage.classList.remove('displayNoneProject');
+    descriptionProject.textContent = 'Zwykly layout pobrany z internetu. Stylowany za pomoca sass zebrany za pomoca gulp.'
+    firstButton.href = "https://alehzahorski.github.io/gangland/";
+    secondButton.href = "https://github.com/AlehZahorski//gangland";
+    secondLi.textContent = "CSS: Sass"
+    thirdLi.textContent = "Javascript: Gulp"
 }
 
 
@@ -125,9 +144,11 @@ closeWindowProject.addEventListener('click', function(){
     firstProjectImage.classList.add('displayNoneProject');
     secondProjectImage.classList.add('displayNoneProject');
     thirdProjectImage.classList.add('displayNoneProject');
+    fourProjectImage.classList.add('displayNoneProject');
 })
 
 btnProject1.addEventListener('click', clickForOpenWindow1);
 btnProject2.addEventListener('click', clickForOpenWindow2);
 btnProject3.addEventListener('click', clickForOpenWindow3);
+btnProject4.addEventListener('click', clickForOpenWindow4);
 
